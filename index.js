736c9a2fc1ea2e21f1303b44b9b40c50098f07af
ls1955 <xxlsxx1955@gmail.com>
2023-07-11 21:34:33 +0800

    Include predefined books
    This commit:
    * Create some books
    * Insert the books into library
    * Call the function to show the books in the page

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ const booksContainer = document.querySelector(".books-container")
 function addBookToLibrary(book) {
     library.push(book)
 }
+
+function showBooks() {
+    booksContainer.innerHTML = ""
+
+    library.forEach(book => {
+        const bookCard = document.createElement("div")
+        bookCard.innerText = book.info()
+        booksContainer.insertAdjacentElement("beforeend", bookCard)
+    })
+}
+
+let bookOne = new Book("Programming Ruby", "Dave Thomas", "111", true)
+let bookTwo = new Book("Refactoring", "Martin Fowler", 700, false)
+let bookThree = new Book("The Rails way", "Obie Femandez", 1000, false)
+
+addBookToLibrary(bookOne)
+addBookToLibrary(bookTwo)
+addBookToLibrary(bookThree)
+
+showBooks()
